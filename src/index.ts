29d042a88a5c86fb558c6064619addc1e321ce18
Saullo Almeida <saullorhamon@gmail.com>
2024-01-16 21:20:25 -0300

    new project with elysia

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.get("/", () => "Hello Elysia")
+.get("/post/:id", ({params: {id}}) => id)
+.post("/post/", ({body, set}) => {
+  set.status = 201
+  return body
+})
+.get("/track/*", () => 'Track Route')
+.get("/tracks", () => {
+  // return new Response(JSON.stringify({
+  //   "tracks": [
+  //     'track 01',
+  //     'track 02',
+  //     'track 03',
+  //   ]
+  // }),{
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // })
+  return {
+    "tracks":[
+          'track 01',
+          'track 02',
+          'track 03',
+        ]
+  }
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
